fc3349d61cd81efee56de6fdeb0092dfafc34952
Octavio Zavala <awsomeboy430@gmail.com>
2023-08-31 13:54:14 -0700

    Update python.py
    Testing_Password

--- python.py
+++ python.py
@@ -1,3 +1,6 @@
+def test_password(x):
+    print(x)
+
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -14,4 +17,6 @@ print("c = {c} and d = {d}".format(c=1, d=2))
 job_list = {'john': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
-    print(f"{name} is a {job}")
+        print(f"{name} is a {job}")
+Password = 'SuperSecretAdminPass'
+test_password(Password)
